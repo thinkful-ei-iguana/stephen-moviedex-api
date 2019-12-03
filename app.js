0dff7bce2185aaa1d2832772e1c4efaf3efa6908
stephenschab <sschab689@gmail.com>
2019-12-03 14:35:52 -0700

    added all get functionality

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+
+const moviedex = require('./moviedex.json');
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
 
-app.use((req, res) => {
-  res.send('Hello, world!');
+  console.log(apiToken, authToken);
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res
+      .status(401)
+      .json({
+        error: 'Unauthorized request'
+      });
+  }
+  next();
 });
+
+function handleGetMovie(req, res) {
+  let response = moviedex;
+
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+
+  if (req.query.country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+
+  if (req.query.avg_vote) {
+    response = response.filter(movie =>
+      Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    );
+  }
+
+  res.json(response);
+}
+
+app.get('/movie', (req, res) => res.json('test'));
 
 const PORT = 8000;
 
