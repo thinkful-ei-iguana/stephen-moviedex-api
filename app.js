bef3cdc24ee95b3274ee398b2ad926e318ab0e66
stephenschab <sschab689@gmail.com>
2019-12-06 13:12:41 -0700

    made predeployment adjustments to files

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const moviedex = require('./moviedex.json');
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 app.use(validateBearerToken);
@@ -53,8 +54,14 @@ function handleGetMovie(req, res) {
 
 app.get('/movie', handleGetMovie);
 
-const PORT = 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
 });
+
+module.exports = app;
